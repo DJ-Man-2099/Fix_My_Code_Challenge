31e010030e3c73f7e7c6937ef8dfb0329face1ad
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-03 22:11:27 +0200

    Tasssk 22

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,8 +5,8 @@
 class Square():
     """ Square class """
 
-    width = 0
-    height = 0
+    __width = 0
+    __height = 0
 
     def __init__(self, *args, **kwargs):
         """ Constructor """
@@ -16,6 +16,34 @@ class Square():
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    @property
+    def width(self):
+        """ Width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Width setter """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -31,7 +59,6 @@ class Square():
 
 
 if __name__ == "__main__":
-
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
