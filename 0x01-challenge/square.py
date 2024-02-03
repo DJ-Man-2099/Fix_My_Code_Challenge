ed4b4429a7d1cdccc6a3e705517014287523014b
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-03 22:41:57 +0200

    Task ???

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,50 +1,33 @@
 #!/usr/bin/python3
-""" Square class """
+""" Square module. """
 
 
 class Square():
-    """ Square class """
-
-    __width = 0
+    """ Square class. """
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Constructor """
-        if len(args) > 0:
-            self.width = args[0]
-        else:
-            for key, value in kwargs.items():
-                if key == "width":
-                    self.width = value
-
-    @property
-    def width(self):
-        """ Width getter """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Width setter """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        """ Square class initialized """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """ Perimeter of the square"""
-        return (self.width * 2) + (self.width * 2)
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String representation"""
-        return "{}/{}".format(self.width, self.width)
+        """ String representation of the square instance """
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
+    """ Create Square instance """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
